bf6d568555420d218376a0f860ae9ad825b1d90c
hhong0326 <hhong0326@naver.com>
2022-05-13 00:07:16 +0900

    Editing 90_SubsetsII.go

--- Golang/Solved/90_SubsetsII.go
+++ Golang/Solved/90_SubsetsII.go
@@ -9,7 +9,7 @@ func subsetsWithDup(nums []int) [][]int {
 
 	sort.Ints(nums)
 	m := map[string][]int{}
-	backtrack([]int{}, nums, "", m)
+	backtrack(&[]int{}, nums, "", m)
 	res := make([][]int, len(m))
 	i := 0
 	for _, s := range m {
@@ -19,21 +19,21 @@ func subsetsWithDup(nums []int) [][]int {
 	return res
 }
 
-func backtrack(current, lefts []int, s string, m map[string][]int) {
+func backtrack(current *[]int, lefts []int, s string, m map[string][]int) {
 	if _, ok := m[s]; ok {
 		return
 	}
 
-	m[s] = current
+	m[s] = *current
 
 	if len(lefts) == 0 {
 		return
 	}
 
 	for i, r := range lefts {
-		nextCurrent := make([]int, len(current))
-		copy(nextCurrent, current)
+		nextCurrent := make([]int, len(*current))
+		copy(nextCurrent, *current)
 		nextCurrent = append(nextCurrent, r)
-		backtrack(nextCurrent, lefts[i+1:], s+strconv.Itoa(r), m)
+		backtrack(&nextCurrent, lefts[i+1:], s+strconv.Itoa(r), m)
 	}
 }
