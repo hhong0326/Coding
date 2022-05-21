a28852d9e1da8ff2f479b6b9ed5b5d0381067b43
hhong0326 <hhong0326@naver.com>
2022-05-21 15:29:57 +0900

    Edit 40_CombinationSumII.go

--- Golang/Solved/40_CombinationSumII.go
+++ Golang/Solved/40_CombinationSumII.go
@@ -1,50 +1,79 @@
 package main
 
 import (
-	"fmt"
 	"sort"
-	"strconv"
 )
 
-func combinationSum2(candidates []int, target int) [][]int {
+// func combinationSum2(candidates []int, target int) [][]int {
 
-	res := [][]int{}
+// 	res := [][]int{}
+
+// 	sort.Ints(candidates)
+// 	m := make(map[string]bool, 0)
+
+// 	backtrack(0, candidates, &res, target, "", []int{}, m)
+
+// 	return res
+// }
+
+// func backtrack(idx int, candidates []int, res *[][]int, t int, s string, arr []int, m map[string]bool) {
+
+// 	if _, ok := m[s]; ok {
+// 		return
+// 	}
+// 	m[s] = true
+// 	if sumArr(arr) == t {
+// 		fmt.Println(arr)
+// 		*res = append(*res, arr)
+// 		return
+// 	} else if sumArr(arr) > t {
+// 		return
+// 	}
+
+// 	for i := idx; i < len(candidates); i++ {
+
+// 		backtrack(i+1, candidates, res, t, s+strconv.Itoa(candidates[i]), append(arr, candidates[i]), m)
+
+// 	}
+
+// }
+
+// func sumArr(arr []int) int {
+// 	sum := 0
+// 	for _, v := range arr {
+// 		sum += v
+// 	}
+
+// 	return sum
+// }
+
+// Others solution
+var res [][]int
+var candis []int
+
+func combinationSum2(candidates []int, target int) [][]int {
+	res = nil
+	candis = candidates
 
 	sort.Ints(candidates)
-	m := make(map[string]bool, 0)
-
-	backtrack(0, candidates, &res, target, "", []int{}, m)
-
+	backtrack(target, nil, 0)
 	return res
 }
 
-func backtrack(idx int, candidates []int, res *[][]int, t int, s string, arr []int, m map[string]bool) {
-
-	if _, ok := m[s]; ok {
-		return
-	}
-	m[s] = true
-	if sumArr(arr) == t {
-		fmt.Println(arr)
-		*res = append(*res, arr)
-		return
-	} else if sumArr(arr) > t {
+func backtrack(target int, cur []int, index int) {
+	if target == 0 {
+		res = append(res, append([]int{}, cur...))
 		return
 	}
 
-	for i := idx; i < len(candidates); i++ {
-
-		backtrack(i+1, candidates, res, t, s+strconv.Itoa(candidates[i]), append(arr, candidates[i]), m)
-
+	if target < 0 {
+		return
 	}
 
-}
-
-func sumArr(arr []int) int {
-	sum := 0
-	for _, v := range arr {
-		sum += v
+	for i := index; i < len(candis); i++ {
+		if i > index && candis[i] == candis[i-1] {
+			continue
+		}
+		backtrack(target-candis[i], append(cur, candis[i]), i+1)
 	}
-
-	return sum
 }
