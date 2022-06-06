be3d9e3235131ad529cd556720e73d8888cadfa6
hhong0326 <hhong0326@naver.com>
2022-06-06 16:51:09 +0900

    Edit 51_NQueens.go

--- Golang/Solved/51_NQueens.go
+++ Golang/Solved/51_NQueens.go
@@ -9,18 +9,21 @@ func solveNQueens(n int) [][]string {
 
 	res := [][]string{}
 
-	queen(0, n, 0, []string{}, &res)
+	queen(0, n, 0, [][]string{}, &res)
 	return res
 
 }
 
-func queen(level int, n int, pos int, board []string, res *[][]string) {
+func queen(level int, n int, pos int, board [][]string, res *[][]string) {
 
 	if !check(level, board, pos) {
 		return
 	} else if level == n {
 		ans := make([]string, len(board))
-		copy(ans, board)
+
+		for i, str := range board {
+			ans[i] = strings.Join(str, "")
+		}
 		(*res) = append(*res, ans)
 		return
 	}
@@ -32,21 +35,21 @@ func queen(level int, n int, pos int, board []string, res *[][]string) {
 				str[j] = "."
 			}
 		}
-		queen(level+1, n, i, append(board, strings.Join(str, "")), res)
+		queen(level+1, n, i, append(board, str), res)
 	}
 }
 
-func check(level int, board []string, pos int) bool {
+func check(level int, board [][]string, pos int) bool {
 	if len(board) <= 1 {
 		return true
 	}
 
 	for i := 0; i < len(board); i++ {
-		if i != level-1 && string(board[i][pos]) == "Q" { // 동일 선상
+		if i != level-1 && board[i][pos] == "Q" { // 동일 선상
 			return false
 		}
 		for j := 0; j < len(board[i]); j++ { // 대각선
-			if i != level-1 && string(board[i][j]) == "Q" && math.Abs(float64(pos-j)/float64((level-1)-i)) == 1.0 {
+			if i != level-1 && board[i][j] == "Q" && math.Abs(float64(pos-j)/float64((level-1)-i)) == 1.0 {
 				return false
 
 			}
@@ -118,4 +121,72 @@ func check(level int, board []string, pos int) bool {
 // 	}
 
 // 	return path
+// }
+
+// 52_NQueens.go Best Solution
+
+// func totalNQueens(n int) int {
+
+//     ans := 0
+//     board := Construct(n)
+
+//     var backtrack func(r int)
+//     backtrack = func(r int) {
+//         if r == n {
+//             ans++
+//             return
+//         }
+
+//         for c := 0; c < n; c++ {
+//             if board.Can(r,c) {
+//                 board.Set(r,c)
+//                 backtrack(r + 1)
+//                 board.Reset(r,c)
+//             }
+//         }
+//     }
+
+//     backtrack(0)
+//     return ans
+// }
+
+// type Board struct {
+//     Boards [][]byte
+//     Row []bool
+//     Col []bool
+//     PosD []bool
+//     NegD []bool
+// }
+
+// func Construct(N int) *Board {
+//     board := make([][]byte, N)
+//     for i := range board {
+//         board[i] = make([]byte, N)
+//     }
+
+//     for i := range board {
+//         for j := range board[i] {
+//             board[i][j] = '.'
+//         }
+//     }
+
+//     return &Board {
+//         Boards: board,
+//         Row: make([]bool, N),
+//         Col: make([]bool, N),
+//         PosD: make([]bool, 2*N),
+//         NegD: make([]bool, 2*N),
+//     }
+// }
+
+// func (b *Board) Set(i, j int) {
+//     b.Boards[i][j], b.Row[i], b.Col[j], b.PosD[i+j], b.NegD[i-j+len(b.Boards)]       = 'Q', true, true, true, true
+// }
+
+// func (b *Board) Reset(i, j int) {
+//     b.Boards[i][j], b.Row[i], b.Col[j], b.PosD[i+j], b.NegD[i-j+len(b.Boards)]       = '.', false, false, false, false
+// }
+
+// func (b *Board) Can(i, j int) bool {
+//     return !b.Row[i] && !b.Col[j] && !b.PosD[i+j] && !b.NegD[i-j+len(b.Boards)]
 // }
