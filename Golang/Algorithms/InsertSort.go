701bae968a4841c654ca785df0a0984dca1e18cd
hhong0326 <hhong0326@naver.com>
2022-06-10 10:34:13 +0900

    Implements InsertSort

---
+++ Golang/Algorithms/InsertSort.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func InsertSort(arr []int) []int {
+
+	for i := 1; i < len(arr); i++ {
+		key := arr[i]
+		j := i - 1
+		for j >= 0 && arr[j] > key {
+			arr[j+1] = arr[j] // 오른쪽 이동
+			j--
+		}
+		arr[j+1] = key
+	}
+	return arr
+}
+
+func main() {
+	arr := make([]int, 10)
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < 10; i++ {
+
+		arr[i] = rand.Intn(100-1) + 1
+	}
+	fmt.Println(arr)
+	fmt.Println(InsertSort(arr))
+}
